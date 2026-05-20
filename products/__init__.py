8f15ce41de2fd6ce82027a1d517b7abb4a211119
garcialopezmanel <mgarcia163@uoc.edu>
2026-05-20 10:47:31 +0200

    users finalizado

--- products/__init__.py
+++ products/__init__.py
@@ -1,1 +1,3 @@
 #Write your code here
+from .food_package import FoodPackage, Wrapping, Bottle, Glass, Box
+from .product import Product, Hamburger, Soda, Drink, HappyMeal
--- users/user.py
+++ users/user.py
@@ -13,7 +13,11 @@ class User(ABC):
 class Cashier(User): 
   def __init__(self,dni:str,name:str,age:int,timeTable:str,salary:float):
     #Write your code here
-    pass      
+    def __init__(self, dni: str, name: str, age: int, till_id: str = "01"):
+        # Inicializa los atributos del padre
+        super().__init__(dni, name, age)
+        # Característica propia
+        self.till_id: str = till_id    
  
   def describe(self):
         return f"Cashier - Name: {self.name}, DNI: {self.dni} , Timetable: {self.timeTable}, Salary: {self.salary}."
@@ -21,8 +25,11 @@ class Cashier(User):
 class Customer(User):
   def __init__(self,dni:str,name:str,age:int,email:str,postalCode:str):
     #Write your code here
-    pass
-
+    def __init__(self, dni: str, name: str, age: int, customer_type: str = "Regular"):
+        # Inicializa los atributos del padre
+        super().__init__(dni, name, age)
+        # Característica propia
+        self.customer_type: str = customer_type
 
   def describe(self):
         return f"Customer - Name: {self.name}, DNI: {self.dni} , Age: {self.age}, Email: {self.email}, Postal Code: {self.postalCode}"
